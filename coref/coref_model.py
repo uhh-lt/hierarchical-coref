1f326214cf12dfc910e9e3d4ede9ed7334b9b68b
Hans Ole Hatzel <hhatzel@gmail.com>
2023-08-22 13:59:38 +0000

    Compatibility with recent transformers version
    While you could just install the older one in theory, it appeared to
    require an (older?) nightly version of rustc. So this is the simpler
    way.

--- coref/coref_model.py
+++ coref/coref_model.py
@@ -363,14 +363,13 @@ class CorefModel:  # pylint: disable=too-many-instance-attributes
 
         # Obtain bert output for selected batches only
         attention_mask = (subwords_batches != self.tokenizer.pad_token_id)
-        out, _ = self.bert(
+        out = self.bert(
             subwords_batches_tensor,
             attention_mask=torch.tensor(
                 attention_mask, device=self.config.device))
-        del _
 
         # [n_subwords, bert_emb]
-        return out[subword_mask_tensor]
+        return out.last_hidden_state[subword_mask_tensor]
 
     def _build_model(self):
         self.bert, self.tokenizer = bert.load_bert(self.config)
